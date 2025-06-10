fb1fb5ab862bf92b177f5cad437927fa30ea6ffb
amirkhalifa285 <amir.kh28@hotmail.com>
2025-06-10 18:41:39 +0300

    Update Cognito configuration for workshop

---
+++ src/aws-exports.js
@@ -0,0 +1,17 @@
+const awsmobile = {
+    "aws_project_region": "eu-west-1",
+    "aws_cognito_region": "eu-west-1", 
+    "aws_user_pools_id": "eu-west-1_GDeB0JroH",
+    "aws_user_pools_web_client_id": "40k61tkjsdecjd86or5o8eecft",
+    "oauth": {},
+    "aws_cloud_logic_custom": [
+        {
+            "name": "WildRydesAPI",
+            "endpoint": "TEMP_API_ENDPOINT",
+            "region": "eu-west-1"
+        }
+    ]
+};
+
+
+export default awsmobile;
