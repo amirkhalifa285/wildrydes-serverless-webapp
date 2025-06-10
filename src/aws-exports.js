12aca7e5d617727dc9454df1aacad2cef49b21d4
amirkhalifa285 <amir.kh28@hotmail.com>
2025-06-10 19:06:23 +0300

    update cognito config

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,8 +1,8 @@
 const awsmobile = {
     "aws_project_region": "eu-west-1",
     "aws_cognito_region": "eu-west-1", 
-    "aws_user_pools_id": "eu-west-1_GDeB0JroH",
-    "aws_user_pools_web_client_id": "40k61tkjsdecjd86or5o8eecft",
+    "aws_user_pools_id": "eu-west-1_k9Tu8FEvi",
+    "aws_user_pools_web_client_id": "3g0t9ndvcdhkti146aqsvdcero",
     "oauth": {},
     "aws_cloud_logic_custom": [
         {
